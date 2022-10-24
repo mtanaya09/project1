ca9dcd4f3ee2f935617da00bb0759941061c7457
Michael <mtanaya09@gmail.com>
2022-10-25 01:09:39 +0800

    pre-layout

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,41 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
+    <div class="container">
+        <div class="row">
+            <div class="col-3 p-5">
+                <img src="https://cdn-icons-png.flaticon.com/512/2202/2202129.png" style="height:150px;"class="rounded-circle">
+            </div>
+            <div class="col-9 pt-5">
+                <div>
+                    <h1>Reflect Practice</h1>
                 </div>
+                <div class="d-flex">
+                    <div><strong>153</strong> posts</div>
+                    <div class="px-3"><strong>23k</strong> followers</div>
+                    <div class="px-3"><strong>212</strong> following</div>
+                </div>
+                <div class="pt-4"><strong>reflect-practice.yey</strong></div>
+                <div>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam
+                    rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt
+                    explicabo.</div>
+                <div><a href="#">www.reflect-practice.yey</a></div>
+            </div>
+        </div>
+
+        <div class="row pt-5">
+            <div class="col-4">
+                <img src="https://images.squarespace-cdn.com/content/v1/5accb37996e76f10e151b0f9/1598543092323-9S8DP0AEE4F7VSBP88XZ/50234103591_81c88c5aec_k.jpg"
+                    class="w-100">
+            </div>
+            <div class="col-4">
+                <img src="https://images.iphonephotographyschool.com/5732/Squre-Composition-01.jpg" class="w-100">
+            </div>
+
+            <div class="col-4">
+                <img src="https://images.squarespace-cdn.com/content/v1/61505c55c04b0a138b337894/1645392354605-KP1A18JWX5FAWEV29EAS/image-asset.png"
+                    class="w-100">
             </div>
         </div>
     </div>
-</div>
 @endsection
